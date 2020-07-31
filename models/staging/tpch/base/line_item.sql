52b9f7a374f3a9b09b338b3f70e5260b187a9ad0
Carly Kaufman <carly@fishtownanalytics.com>
2020-07-31 21:02:31 +0000

    finished adding sources

--- models/staging/tpch/base/line_item.sql
+++ models/staging/tpch/base/line_item.sql
@@ -23,7 +23,7 @@ renamed as (
         l_receiptdate as receipt_date,
         l_shipinstruct as ship_instructions,
         l_shipmode as ship_mode,
-        l_comment
+        l_comment as comment
 
     from source
 
