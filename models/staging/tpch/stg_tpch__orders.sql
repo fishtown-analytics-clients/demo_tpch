a3456905efbd053b3ff8d811e25412ce91f5c9b3
Carly Kaufman <carly@fishtownanalytics.com>
2020-07-31 16:34:46 -0600

    Add aliasing

--- models/staging/tpch/stg_tpch__orders.sql
+++ models/staging/tpch/stg_tpch__orders.sql
@@ -8,15 +8,15 @@ with source as (
 renamed as (
 
     select
-        o_orderkey,
-        o_custkey,
-        o_orderstatus,
-        o_totalprice,
-        o_orderdate,
-        o_orderpriority,
-        o_clerk,
-        o_shippriority,
-        o_comment
+        o_orderkey as order_key,
+        o_custkey as customer_key,
+        o_orderstatus as order_status,
+        o_totalprice as total_price,
+        o_orderdate as order_date,
+        o_orderpriority as order_priority,
+        o_clerk as clerk,
+        o_shippriority as ship_priority,
+        o_comment as comment
 
     from source
 
