66ed4297b9218cd80e1ab6b8570137a6c0a8eeb1
Luther Hill <luther@fishtownanalytics.com>
2020-08-17 19:30:21 +0000

    updated tags and external tables

--- models/staging/external_stage/stg_jaffle_shop_customers.sql
+++ models/staging/external_stage/stg_jaffle_shop_customers.sql
@@ -1,12 +1,18 @@
+{{ config(
+  enabled= true,
+  tags = ["customers"]
+) }}
+
 with source as (
 
     select * from {{ source('jaffle_shop', 'customers') }}
 
-),
+)
 
 select
     id,
     first_name,
-    last_name
+    last_name,
+    email
 
 from source
