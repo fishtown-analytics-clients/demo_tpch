0b0f04ae8b2884a5758a4aac0fecd9b93f555f25
Erin Vaughan <erin@fishtownanalytics.com>
2020-10-14 18:13:58 +0000

    update line items

--- models/staging/tpch/order_item.sql
+++ models/staging/tpch/order_item.sql
@@ -3,58 +3,65 @@
         materialized = 'view'
     )
 }}
-with orders as (
+
+with 
+
+orders as (
     
     select * from {{ ref('stg_tpch__orders') }}
 
 ),
-line_item as (
 
-    select * from {{ ref('base_tpch__line_item') }}
+line_items as (
+
+    select * from {{ ref('stg_tpch__line_items') }}
+
+),
+
+final as (
+
+    select 
+        {{ dbt_utils.surrogate_key(['orders.order_key','line_items.line_number']) }} as order_item_key,
+        orders.order_key,
+        orders.order_date,
+        orders.customer_key,
+        orders.status_code as order_status_code,
+        
+        line_items.part_key,
+        line_items.supplier_key,
+        line_items.return_flag,
+        
+        line_items.line_number,
+        line_items.status_code as order_item_status_code,
+        line_items.ship_date,
+        line_items.commit_date,
+        line_items.receipt_date,
+        line_items.ship_mode,
+        line_items.extended_price,
+        line_items.quantity,
+        
+        -- extended_price is actually the line item total,
+        -- so we back out the extended price per item
+        (line_items.extended_price/nullif(line_items.quantity, 0)){{ money() }} as base_price,
+        line_items.discount_percentage,
+        (base_price * (1 - line_items.discount_percentage)){{ money() }} as discounted_price,
+
+        line_items.extended_price as gross_item_sales_amount,
+        (line_items.extended_price * (1 - line_items.discount_percentage)){{ money() }} as discounted_item_sales_amount,
+        -- We model discounts as negative amounts
+        (-1 * line_items.extended_price * line_items.discount_percentage){{ money() }} as item_discount_amount,
+        line_items.tax_rate,
+        ((gross_item_sales_amount + item_discount_amount) * line_items.tax_rate){{ money() }} as item_tax_amount,
+        (
+            gross_item_sales_amount + 
+            item_discount_amount + 
+            item_tax_amount
+        ){{ money() }} as net_item_sales_amount
+
+    from orders
+    inner join line_items
+        on orders.order_key = line_items.order_key
 
 )
-select 
 
-    {{ dbt_utils.surrogate_key(['orders.order_key','line_item.line_number']) }} as order_item_key,
-    orders.order_key,
-    orders.order_date,
-    orders.customer_key,
-    orders.status_code as order_status_code,
-    
-    line_item.part_key,
-    line_item.supplier_key,
-    line_item.return_flag,
-    
-    line_item.line_number,
-    line_item.status_code as order_item_status_code,
-    line_item.ship_date,
-    line_item.commit_date,
-    line_item.receipt_date,
-    line_item.ship_mode,
-    line_item.extended_price,
-    line_item.quantity,
-    
-    -- extended_price is actually the line item total,
-    -- so we back out the extended price per item
-    (line_item.extended_price/nullif(line_item.quantity, 0)){{ money() }} as base_price,
-    line_item.discount_percentage,
-    (base_price * (1 - line_item.discount_percentage)){{ money() }} as discounted_price,
-
-    line_item.extended_price as gross_item_sales_amount,
-    (line_item.extended_price * (1 - line_item.discount_percentage)){{ money() }} as discounted_item_sales_amount,
-    -- We model discounts as negative amounts
-    (-1 * line_item.extended_price * line_item.discount_percentage){{ money() }} as item_discount_amount,
-    line_item.tax_rate,
-    ((gross_item_sales_amount + item_discount_amount) * line_item.tax_rate){{ money() }} as item_tax_amount,
-    (
-        gross_item_sales_amount + 
-        item_discount_amount + 
-        item_tax_amount
-    ){{ money() }} as net_item_sales_amount
-
-from
-    orders
-inner join line_item
-        on orders.order_key = line_item.order_key
-order by
-    orders.order_date
+select * from final
